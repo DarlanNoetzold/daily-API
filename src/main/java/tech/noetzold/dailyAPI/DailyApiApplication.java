0ac5f6f4a938f8a5df16f0246df91abc200ca42d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-16 22:08:42 -0300

    Initial commit

---
+++ src/main/java/tech/noetzold/dailyAPI/DailyApiApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.dailyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyApiApplication.class, args);
+	}
+
+}
