280f78907a960dde186a1f6632c2f40b4f721268
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-20 11:32:58 -0300

    Update Email.java

--- src/main/java/tech/noetzold/dailyAPI/model/Email.java
+++ src/main/java/tech/noetzold/dailyAPI/model/Email.java
@@ -1,4 +1,24 @@
 package tech.noetzold.dailyAPI.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Email {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String sender;
+    private String subject;
+    private String content;
 }
