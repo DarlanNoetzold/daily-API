616d9bb4a538d6f2a49adae269f1302704c925c2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-21 08:08:23 -0300

    Create TaskService.java

---
+++ src/main/java/tech/noetzold/dailyAPI/service/TaskService.java
@@ -0,0 +1,41 @@
+package tech.noetzold.dailyAPI.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.noetzold.dailyAPI.model.Task;
+import tech.noetzold.dailyAPI.repository.TaskRepository;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    TaskRepository taskRepository;
+
+    @Transactional
+    public List<Task> findAllTask(){
+        return taskRepository.findAll();
+    }
+
+    @Transactional
+    public Task findTaskById(Long id){
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    @Transactional
+    public Task updateTask(Long id, Task task){
+        task.setId(id);
+        return taskRepository.save(task);
+    }
+
+    @Transactional
+    public Task saveTask(Task task){
+        return taskRepository.save(task);
+    }
+
+    @Transactional
+    public void deleteTask(Long id){
+        taskRepository.deleteById(id);
+    }
+}
