8fd368d15812ee9a213c037a8eabb3f8441616a5
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-23 08:59:40 -0300

    Update NewsFeignClient.java

--- src/main/java/tech/noetzold/dailyAPI/client/NewsFeignClient.java
+++ src/main/java/tech/noetzold/dailyAPI/client/NewsFeignClient.java
@@ -3,7 +3,7 @@ package tech.noetzold.dailyAPI.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import tech.noetzold.dailyAPI.model.TabnewsResponse;
+import tech.noetzold.dailyAPI.model.dto.TabnewsResponse;
 
 import java.util.List;
 
