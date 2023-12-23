999f102cc08229c35e28159854a699a6f88cd9a5
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-23 08:59:44 -0300

    Update WeatherFeignClient.java

--- src/main/java/tech/noetzold/dailyAPI/client/WeatherFeignClient.java
+++ src/main/java/tech/noetzold/dailyAPI/client/WeatherFeignClient.java
@@ -3,7 +3,7 @@ package tech.noetzold.dailyAPI.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import tech.noetzold.dailyAPI.model.WeatherResponse;
+import tech.noetzold.dailyAPI.model.dto.WeatherResponse;
 
 @FeignClient(name = "weather-api", url = "https://api.openweathermap.org/data/2.5")
 public interface WeatherFeignClient {
