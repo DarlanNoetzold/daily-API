7365a7bdae1d05a76c1d1dde1829453380bb3cc0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-23 08:59:37 -0300

    Update DollarFeignClient.java

--- src/main/java/tech/noetzold/dailyAPI/client/DollarFeignClient.java
+++ src/main/java/tech/noetzold/dailyAPI/client/DollarFeignClient.java
@@ -2,7 +2,7 @@ package tech.noetzold.dailyAPI.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import tech.noetzold.dailyAPI.model.DollarResponse;
+import tech.noetzold.dailyAPI.model.dto.DollarResponse;
 
 @FeignClient(name = "dollar-api", url = "https://economia.awesomeapi.com.br")
 public interface DollarFeignClient {
