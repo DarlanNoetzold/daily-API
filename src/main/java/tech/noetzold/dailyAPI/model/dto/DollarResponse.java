da9222e5ce1e0df44a92eabf097f0bc1deab577a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-23 08:59:49 -0300

    Create DollarResponse.java

---
+++ src/main/java/tech/noetzold/dailyAPI/model/dto/DollarResponse.java
@@ -0,0 +1,20 @@
+package tech.noetzold.dailyAPI.model.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import tech.noetzold.dailyAPI.model.DollarValue;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class DollarResponse {
+
+    private Long id;
+    @JsonProperty("USDBRL")
+    private DollarValue USDBRL;
+}
+
