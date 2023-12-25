6e769fbcf8d8ea542a3c794a92196d10e2c97cf6
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-24 22:28:45 -0300

    Update DailyApiApplication.java

--- src/main/java/tech/noetzold/dailyAPI/DailyApiApplication.java
+++ src/main/java/tech/noetzold/dailyAPI/DailyApiApplication.java
@@ -2,8 +2,10 @@ package tech.noetzold.dailyAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackageClasses=tech.noetzold.dailyAPI.client.MoviesFeignClient.class)
 public class DailyApiApplication {
 
 	public static void main(String[] args) {
