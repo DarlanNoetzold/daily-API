f4b118dcc32b1a6f88ebc0264bf4046798cf39c7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-25 20:24:47 -0300

    Update AnimeFeignClient.java

--- src/main/java/tech/noetzold/dailyAPI/client/AnimeFeignClient.java
+++ src/main/java/tech/noetzold/dailyAPI/client/AnimeFeignClient.java
@@ -1,4 +1,12 @@
 package tech.noetzold.dailyAPI.client;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import tech.noetzold.dailyAPI.model.dto.AnimeSeasonResponse;
+
+@FeignClient(name = "animeClient", url = "https://api.jikan.moe/v4")
 public interface AnimeFeignClient {
+    @GetMapping("/seasons/{year}/{season}")
+    AnimeSeasonResponse getSeasonAnimes(@PathVariable("year") int year, @PathVariable("season") String season);
 }
