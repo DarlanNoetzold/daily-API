d1b6fdf0949cc43efafcff60a5fc018c27856412
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-25 20:25:16 -0300

    Update TVShowController.java

--- src/main/java/tech/noetzold/dailyAPI/controller/TVShowController.java
+++ src/main/java/tech/noetzold/dailyAPI/controller/TVShowController.java
@@ -1,5 +1,6 @@
 package tech.noetzold.dailyAPI.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +13,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/tvshows")
 public class TVShowController {
-    private final TVShowService tvShowService;
 
-    public TVShowController(TVShowService tvShowService) {
-        this.tvShowService = tvShowService;
-    }
+    @Autowired
+    private TVShowService tvShowService;
 
     @GetMapping("/trending")
     public ResponseEntity<List<TVShow>> getTrendingTVShows() {
