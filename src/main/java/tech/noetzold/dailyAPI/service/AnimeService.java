feb955f014692c1a4fc962142dd63c84a07f2da9
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-25 20:25:26 -0300

    Create AnimeService.java

---
+++ src/main/java/tech/noetzold/dailyAPI/service/AnimeService.java
@@ -0,0 +1,27 @@
+package tech.noetzold.dailyAPI.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.noetzold.dailyAPI.client.AnimeFeignClient;
+import tech.noetzold.dailyAPI.model.Anime;
+import tech.noetzold.dailyAPI.model.dto.AnimeSeasonResponse;
+import tech.noetzold.dailyAPI.repository.AnimeRepository;
+
+import java.util.List;
+
+@Service
+public class AnimeService {
+    private final AnimeFeignClient animeClient;
+    private final AnimeRepository animeRepository;
+
+    @Autowired
+    public AnimeService(AnimeFeignClient animeClient, AnimeRepository animeRepository) {
+        this.animeClient = animeClient;
+        this.animeRepository = animeRepository;
+    }
+
+    public List<Anime> getSeasonAnimes(int year, String season) {
+        AnimeSeasonResponse response = animeClient.getSeasonAnimes(year, season);
+        return animeRepository.saveAll(response.getData());
+    }
+}
