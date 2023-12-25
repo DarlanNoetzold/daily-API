a6c6e1b5048814835029046e66436c87ac716a07
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-24 22:28:48 -0300

    Update Movie.java

--- src/main/java/tech/noetzold/dailyAPI/model/Movie.java
+++ src/main/java/tech/noetzold/dailyAPI/model/Movie.java
@@ -1,5 +1,6 @@
 package tech.noetzold.dailyAPI.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.util.Date;
@@ -16,21 +17,25 @@ public class Movie {
     @Column(name = "adult")
     private Boolean adult;
 
+    @JsonProperty("backdrop_path")
     @Column(name = "backdrop_path")
     private String backdropPath;
 
     @Column(name = "original_language")
     private String originalLanguage;
 
+    @JsonProperty("original_title")
     @Column(name = "original_title")
     private String originalTitle;
 
     @Column(name = "overview", length = 1000)
     private String overview;
 
+    @JsonProperty("poster_path")
     @Column(name = "poster_path")
     private String posterPath;
 
+    @JsonProperty("media_type")
     @Column(name = "media_type")
     private String mediaType;
 
@@ -42,6 +47,7 @@ public class Movie {
     @Column(name = "popularity")
     private Double popularity;
 
+    @JsonProperty("release_date")
     @Column(name = "release_date")
     @Temporal(TemporalType.DATE)
     private Date releaseDate;
@@ -49,9 +55,11 @@ public class Movie {
     @Column(name = "video")
     private Boolean video;
 
+    @JsonProperty("vote_average")
     @Column(name = "vote_average")
     private Double voteAverage;
 
+    @JsonProperty("vote_count")
     @Column(name = "vote_count")
     private Integer voteCount;
 
