6c3cd117e2b6c7694f29427e526c14baacf28899
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-26 10:20:08 -0300

    Create Game.java

---
+++ src/main/java/tech/noetzold/dailyAPI/model/Game.java
@@ -0,0 +1,51 @@
+package tech.noetzold.dailyAPI.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "game")
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @JsonProperty("slug")
+    @Column(name = "slug")
+    private String slug;
+
+    @JsonProperty("name")
+    @Column(name = "name")
+    private String name;
+
+    @ElementCollection
+    private List<String> platforms;
+
+    @JsonProperty("released")
+    @Column(name = "released")
+    private String released;
+
+    @JsonProperty("tba")
+    @Column(name = "tba")
+    private Boolean tba;
+
+    @JsonProperty("background_image")
+    @Column(name = "background_image")
+    private String backgroundImage;
+
+    @JsonProperty("rating")
+    @Column(name = "rating")
+    private Double rating;
+
+    @JsonProperty("rating_top")
+    @Column(name = "rating_top")
+    private Integer ratingTop;
+
+    @JsonProperty("ratings_count")
+    @Column(name = "ratings_count")
+    private Integer ratingsCount;
+}
