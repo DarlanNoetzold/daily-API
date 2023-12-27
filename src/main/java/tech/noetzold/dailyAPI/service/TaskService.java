b5cd9f164f458d1672063ed5492af58902d96ccf
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-27 17:27:13 -0300

    Update TaskService.java

--- src/main/java/tech/noetzold/dailyAPI/service/TaskService.java
+++ src/main/java/tech/noetzold/dailyAPI/service/TaskService.java
@@ -2,6 +2,7 @@ package tech.noetzold.dailyAPI.service;
 
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import tech.noetzold.dailyAPI.model.Task;
 import tech.noetzold.dailyAPI.repository.TaskRepository;
