a508624e958acc3ebaca4a6a261a7038372733b2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-27 17:16:02 -0300

    Update WeatherPredictService.java

--- src/main/java/tech/noetzold/dailyAPI/service/WeatherPredictService.java
+++ src/main/java/tech/noetzold/dailyAPI/service/WeatherPredictService.java
@@ -3,14 +3,20 @@ package tech.noetzold.dailyAPI.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.noetzold.dailyAPI.client.WeatherFeignClient;
+import tech.noetzold.dailyAPI.model.WeatherPredict;
 import tech.noetzold.dailyAPI.model.dto.WeatherResponse;
+import tech.noetzold.dailyAPI.repository.WeatherPredictRepository;
 
 @Service
 public class WeatherPredictService {
     @Autowired
     private WeatherFeignClient weatherFeignClient;
 
-    public WeatherResponse getWeather(String lat, String lon, String apiKey) {
-        return weatherFeignClient.getWeather(lat, lon, apiKey);
+    @Autowired
+    private WeatherPredictRepository weatherPredictRepository;
+
+    public WeatherPredict getWeather(String lat, String lon, String apiKey) {
+        WeatherResponse weatherResponse = weatherFeignClient.getWeather(lat, lon, apiKey);
+        return weatherPredictRepository.save(weatherResponse.getMain());
     }
 }
