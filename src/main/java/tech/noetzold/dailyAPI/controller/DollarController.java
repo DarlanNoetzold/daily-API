44b3529eb40fc9f62ce7afaa12b78cf65dbcb2b5
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-27 17:15:20 -0300

    Create DollarController.java

---
+++ src/main/java/tech/noetzold/dailyAPI/controller/DollarController.java
@@ -0,0 +1,9 @@
+package tech.noetzold.dailyAPI.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dollars")
+public class DollarController {
+}
