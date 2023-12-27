e5a01e7a84a4487ca4fb9bb2ff35eb3c7301ee88
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-27 17:15:25 -0300

    Create NewsController.java

---
+++ src/main/java/tech/noetzold/dailyAPI/controller/NewsController.java
@@ -0,0 +1,10 @@
+package tech.noetzold.dailyAPI.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/news")
+public class NewsController {
+}
