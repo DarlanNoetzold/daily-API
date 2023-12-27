879704cd1d07aa22a2f7f2853183d591b96ba9f5
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-27 19:54:14 -0300

    Update DollarController.java

--- src/main/java/tech/noetzold/dailyAPI/controller/DollarController.java
+++ src/main/java/tech/noetzold/dailyAPI/controller/DollarController.java
@@ -1,9 +1,25 @@
 package tech.noetzold.dailyAPI.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.dailyAPI.model.DollarValue;
+import tech.noetzold.dailyAPI.service.DollarService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/dollars")
 public class DollarController {
+
+    @Autowired
+    DollarService dollarService;
+
+    @GetMapping("/value")
+    public ResponseEntity<DollarValue> getDollar() {
+        DollarValue dollarValue = dollarService.getDolarRate();
+        return ResponseEntity.ok(dollarValue);
+    }
 }
