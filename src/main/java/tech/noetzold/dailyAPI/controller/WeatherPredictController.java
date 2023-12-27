979d3a827784ac9b8d6f5122f0e1bc5750290638
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-27 19:54:18 -0300

    Update WeatherPredictController.java

--- src/main/java/tech/noetzold/dailyAPI/controller/WeatherPredictController.java
+++ src/main/java/tech/noetzold/dailyAPI/controller/WeatherPredictController.java
@@ -1,10 +1,26 @@
 package tech.noetzold.dailyAPI.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.dailyAPI.model.WeatherPredict;
+import tech.noetzold.dailyAPI.service.WeatherPredictService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/weather")
 public class WeatherPredictController {
+
+    @Autowired
+    WeatherPredictService weatherPredictService;
+
+    @GetMapping("/now")
+    public ResponseEntity<WeatherPredict> getNews() {
+        WeatherPredict weatherPredict = weatherPredictService.getWeather();
+        return ResponseEntity.ok(weatherPredict);
+    }
 }
