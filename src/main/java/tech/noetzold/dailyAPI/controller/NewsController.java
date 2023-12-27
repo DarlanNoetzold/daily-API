7a4f43be0abe4f7c19b38f8112578805899ae525
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-27 19:54:16 -0300

    Update NewsController.java

--- src/main/java/tech/noetzold/dailyAPI/controller/NewsController.java
+++ src/main/java/tech/noetzold/dailyAPI/controller/NewsController.java
@@ -1,10 +1,26 @@
 package tech.noetzold.dailyAPI.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tech.noetzold.dailyAPI.model.dto.TabnewsResponse;
+import tech.noetzold.dailyAPI.service.NewsService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/news")
 public class NewsController {
+
+    @Autowired
+    NewsService newsService;
+
+    @GetMapping("/trending")
+    public ResponseEntity<List<TabnewsResponse>> getNews() {
+        List<TabnewsResponse> news = newsService.getNews();
+        return ResponseEntity.ok(news);
+    }
 }
