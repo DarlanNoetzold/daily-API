9dc0f5395900e37f9c3c481376fc821d5c3ac24d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-27 17:15:58 -0300

    Update TVShowService.java

--- src/main/java/tech/noetzold/dailyAPI/service/TVShowService.java
+++ src/main/java/tech/noetzold/dailyAPI/service/TVShowService.java
@@ -1,5 +1,6 @@
 package tech.noetzold.dailyAPI.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.noetzold.dailyAPI.client.TVFeignClient;
 import tech.noetzold.dailyAPI.model.TVShow;
@@ -10,13 +11,12 @@ import java.util.List;
 
 @Service
 public class TVShowService {
-    private final TVFeignClient tvClient;
-    private final TVShowRepository tvShowRepository;
+    
+    @Autowired
+    private TVFeignClient tvClient;
 
-    public TVShowService(TVFeignClient tvClient, TVShowRepository tvShowRepository) {
-        this.tvClient = tvClient;
-        this.tvShowRepository = tvShowRepository;
-    }
+    @Autowired
+    private TVShowRepository tvShowRepository;
 
     public List<TVShow> getTrendingTVShows() {
         TrendingTVShowsResponse response = tvClient.getTrendingTVShows("Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MzExYzAxYWYwMmVkZTNiZTg3NDY3M2E5NGM1ZmU0ZiIsInN1YiI6IjY1ODdhN2ZjNWFiYTMyNjZiMGI5ODhiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ToNV7Fr8QaoWiaD6d2fAeVbgK7v6WOL62AZtdGGZ5Mk");
