3f4c455aa04acd4eb11bd96ca9b2fb2da3631f8c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-28 08:53:49 -0300

    Update TrendingTVShowsResponse.java

--- src/main/java/tech/noetzold/dailyAPI/model/dto/TrendingTVShowsResponse.java
+++ src/main/java/tech/noetzold/dailyAPI/model/dto/TrendingTVShowsResponse.java
@@ -3,10 +3,11 @@ package tech.noetzold.dailyAPI.model.dto;
 import lombok.Data;
 import tech.noetzold.dailyAPI.model.TVShow;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class TrendingTVShowsResponse {
+public class TrendingTVShowsResponse implements Serializable {
     private Integer page;
     private List<TVShow> results;
     private Integer totalPages;
