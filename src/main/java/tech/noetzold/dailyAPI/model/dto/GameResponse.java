1f2f40b3c80fadce19e9cb56479c4d3ace173719
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-28 08:53:41 -0300

    Update GameResponse.java

--- src/main/java/tech/noetzold/dailyAPI/model/dto/GameResponse.java
+++ src/main/java/tech/noetzold/dailyAPI/model/dto/GameResponse.java
@@ -3,10 +3,11 @@ package tech.noetzold.dailyAPI.model.dto;
 import lombok.Data;
 import tech.noetzold.dailyAPI.model.Game;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class GameResponse {
+public class GameResponse implements Serializable {
     private Integer count;
     private String next;
     private String previous;
