6ce28a2dbe5e4de08c2e8dc6258f85bc45d6c417
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-28 08:53:34 -0300

    Update AnimeSeasonResponse.java

--- src/main/java/tech/noetzold/dailyAPI/model/dto/AnimeSeasonResponse.java
+++ src/main/java/tech/noetzold/dailyAPI/model/dto/AnimeSeasonResponse.java
@@ -3,9 +3,10 @@ package tech.noetzold.dailyAPI.model.dto;
 import lombok.Data;
 import tech.noetzold.dailyAPI.model.Anime;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
-public class AnimeSeasonResponse {
+public class AnimeSeasonResponse implements Serializable {
     private List<Anime> data;
 }
