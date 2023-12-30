76ec930f92126df29257c7c171413c759e88b196
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-30 11:04:53 -0300

    Update WeatherPredictController.java

--- src/main/java/tech/noetzold/dailyAPI/controller/WeatherPredictController.java
+++ src/main/java/tech/noetzold/dailyAPI/controller/WeatherPredictController.java
@@ -1,6 +1,7 @@
 package tech.noetzold.dailyAPI.controller;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +17,12 @@ public class WeatherPredictController {
     @Autowired
     WeatherPredictService weatherPredictService;
 
+    private static final Logger logger = LoggerFactory.getLogger(AnimeController.class);
+
     @GetMapping("/now")
     public ResponseEntity<WeatherPredict> getWeather() {
         WeatherPredict weatherPredict = weatherPredictService.getWeather();
+        logger.info("Get Weather infos");
         return ResponseEntity.ok(weatherPredict);
     }
 }
