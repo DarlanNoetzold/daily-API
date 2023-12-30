0d298a8fe23be4492134776d89524075c489b3e4
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-30 11:04:46 -0300

    Update NewsController.java

--- src/main/java/tech/noetzold/dailyAPI/controller/NewsController.java
+++ src/main/java/tech/noetzold/dailyAPI/controller/NewsController.java
@@ -1,6 +1,7 @@
 package tech.noetzold.dailyAPI.controller;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +19,11 @@ public class NewsController {
     @Autowired
     NewsService newsService;
 
+    private static final Logger logger = LoggerFactory.getLogger(AnimeController.class);
+
     @GetMapping("/trending")
     public ResponseEntity<List<TabnewsResponse>> getNews() {
+        logger.info("Get news");
         List<TabnewsResponse> news = newsService.getNews();
         return ResponseEntity.ok(news);
     }
