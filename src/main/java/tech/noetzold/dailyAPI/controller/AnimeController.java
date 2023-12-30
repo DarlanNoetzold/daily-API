8a65f62e4392367e7a60f043b872a983a589a762
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-30 11:04:33 -0300

    Update AnimeController.java

--- src/main/java/tech/noetzold/dailyAPI/controller/AnimeController.java
+++ src/main/java/tech/noetzold/dailyAPI/controller/AnimeController.java
@@ -1,5 +1,7 @@
 package tech.noetzold.dailyAPI.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +20,12 @@ public class AnimeController {
     @Autowired
     private AnimeService animeService;
 
+    private static final Logger logger = LoggerFactory.getLogger(AnimeController.class);
+
     @GetMapping("/season/{year}/{season}")
     public ResponseEntity<List<Anime>> getSeasonAnimes(@PathVariable int year, @PathVariable String season) {
         List<Anime> animes = animeService.getSeasonAnimes(year, season);
+        logger.info("Returning animes");
         return ResponseEntity.ok(animes);
     }
 }
