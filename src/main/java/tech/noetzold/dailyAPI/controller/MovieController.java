fbf5061bbd452e7e7300e09885171a87dfbd803d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-30 11:04:44 -0300

    Update MovieController.java

--- src/main/java/tech/noetzold/dailyAPI/controller/MovieController.java
+++ src/main/java/tech/noetzold/dailyAPI/controller/MovieController.java
@@ -1,6 +1,9 @@
 package tech.noetzold.dailyAPI.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +18,12 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
+    private static final Logger logger = LoggerFactory.getLogger(AnimeController.class);
+
     @GetMapping("/trending")
-    public List<Movie> getTrendingMovies() {
-        return movieService.getTrendingMovies();
+    public ResponseEntity<List<Movie>> getTrendingMovies() {
+        List<Movie> movies = movieService.getTrendingMovies();
+        logger.info("Get movies");
+        return ResponseEntity.ok(movies);
     }
 }
