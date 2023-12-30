629a50cae2ae468c16dd8877cb9d61ebd4b8cca8
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2023-12-30 11:04:35 -0300

    Update CacheController.java

--- src/main/java/tech/noetzold/dailyAPI/controller/CacheController.java
+++ src/main/java/tech/noetzold/dailyAPI/controller/CacheController.java
@@ -1,6 +1,7 @@
 package tech.noetzold.dailyAPI.controller;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.CacheManager;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,12 @@ public class CacheController {
     @Autowired
     private CacheManager cacheManager;
 
+    private static final Logger logger = LoggerFactory.getLogger(AnimeController.class);
+
     @GetMapping
     public void clearAllCaches() {
         for(String name : cacheManager.getCacheNames()) {
+            logger.info("Cleaning cache - " + name);
             cacheManager.getCache(name).clear();
         }
     }
