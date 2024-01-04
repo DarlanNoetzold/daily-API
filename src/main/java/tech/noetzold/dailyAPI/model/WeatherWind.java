c5f446e1a3127bdf0612df59cb03b59b39ba01a2
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-04 09:39:04 -0300

    Create WeatherWind.java

---
+++ src/main/java/tech/noetzold/dailyAPI/model/WeatherWind.java
@@ -0,0 +1,27 @@
+package tech.noetzold.dailyAPI.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class WeatherWind implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id_local;
+    private Double speed;
+    private Integer deg;
+    private Double gust;
+
+}
