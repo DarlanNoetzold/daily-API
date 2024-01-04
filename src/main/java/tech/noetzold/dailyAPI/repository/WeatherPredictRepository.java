be32f44a07aa68c88cd2dfbb0b8def5aeb2d3fc7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-04 13:52:58 -0300

    Update WeatherPredictRepository.java

--- src/main/java/tech/noetzold/dailyAPI/repository/WeatherPredictRepository.java
+++ src/main/java/tech/noetzold/dailyAPI/repository/WeatherPredictRepository.java
@@ -2,6 +2,7 @@ package tech.noetzold.dailyAPI.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.noetzold.dailyAPI.model.WeatherPredict;
+import tech.noetzold.dailyAPI.model.dto.WeatherResponse;
 
-public interface WeatherPredictRepository extends JpaRepository<WeatherPredict, Long> {
+public interface WeatherPredictRepository extends JpaRepository<WeatherResponse, Long> {
 }
