86e96f2136544bff5dd5d84805538197d64abdb3
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-08 07:55:01 -0300

    Create WebConfig.java

---
+++ src/main/java/tech/noetzold/dailyAPI/util/WebConfig.java
@@ -0,0 +1,29 @@
+package tech.noetzold.dailyAPI.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedMethods(List.of("*"));
+
+        source.registerCorsConfiguration("/**", config);
+        source.registerCorsConfiguration("/actuator/metrics", config);
+
+        return new CorsFilter(source);
+    }
+}
