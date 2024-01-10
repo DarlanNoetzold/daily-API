1491402b26aa4c2ec3fafff5f4b959a0fd7a469c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-10 08:50:16 -0300

    Create TrackingService.java

---
+++ src/main/java/tech/noetzold/dailyAPI/service/TrackingService.java
@@ -0,0 +1,33 @@
+package tech.noetzold.dailyAPI.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import tech.noetzold.dailyAPI.client.PackageFeignClient;
+import tech.noetzold.dailyAPI.model.TrackingResponse;
+import tech.noetzold.dailyAPI.repository.TrackingRepository;
+
+
+@Service
+@Cacheable("tracking")
+public class TrackingService {
+
+    @Autowired
+    private PackageFeignClient packageFeignClient;
+
+    @Autowired
+    private TrackingRepository trackingRepository;
+
+    @Value("${spring.tracking.token}")
+    private String token;
+
+    @Value("${spring.tracking.user}")
+    private String user;
+
+    public TrackingResponse getTracking(String cod){
+        TrackingResponse trackingResponse = packageFeignClient.getPackage(user, token, cod);
+        trackingRepository.save(trackingResponse);
+        return trackingResponse;
+    }
+}
