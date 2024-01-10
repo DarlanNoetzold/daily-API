cd1b58a41f6bf390ec96443e4597151c5b20f52d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-10 08:49:59 -0300

    Update MovieController.java

--- src/main/java/tech/noetzold/dailyAPI/controller/MovieController.java
+++ src/main/java/tech/noetzold/dailyAPI/controller/MovieController.java
@@ -20,7 +20,7 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
-    private static final Logger logger = LoggerFactory.getLogger(AnimeController.class);
+    private static final Logger logger = LoggerFactory.getLogger(MovieController.class);
 
     @GetMapping("/trending")
     public ResponseEntity<List<Movie>> getTrendingMovies() {
