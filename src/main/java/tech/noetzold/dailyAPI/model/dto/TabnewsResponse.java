6e4f10bcf8ca64a4d66e0e8a99792c0775ca2acb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-11 08:44:29 -0300

    Update TabnewsResponse.java

--- src/main/java/tech/noetzold/dailyAPI/model/dto/TabnewsResponse.java
+++ src/main/java/tech/noetzold/dailyAPI/model/dto/TabnewsResponse.java
@@ -1,15 +1,13 @@
 package tech.noetzold.dailyAPI.model.dto;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Data
 @NoArgsConstructor
@@ -34,4 +32,6 @@ public class TabnewsResponse implements Serializable {
     private Integer tabcoins;
     private String owner_username;
     private Integer children_deep_count;
+    @Column(name = "request_date")
+    private Date requestDate;
 }
