484eaa5d446f84faac1b1f54ade215ed3ff358d7
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-15 18:19:00 -0300

    Create HomeAssistantFeignClient.java

---
+++ src/main/java/tech/noetzold/dailyAPI/client/HomeAssistantFeignClient.java
@@ -0,0 +1,15 @@
+package tech.noetzold.dailyAPI.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import tech.noetzold.dailyAPI.model.dto.TrendingMoviesResponse;
+
+@FeignClient(name = "homeAssistantClient", url = "http://192.168.18.75:8123/api")
+public interface HomeAssistantFeignClient {
+
+    @GetMapping("/")
+    String isHomeAssistentUp(@RequestHeader("Authorization") String bearerToken);
+    @GetMapping("/trending/movie/day")
+    TrendingMoviesResponse getTrendingMovies(@RequestHeader("Authorization") String bearerToken);
+}
