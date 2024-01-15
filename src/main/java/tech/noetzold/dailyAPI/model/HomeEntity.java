d1b4a971c03252b451db3a865693d966ad6cc5fe
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-15 18:19:10 -0300

    Create HomeEntity.java

---
+++ src/main/java/tech/noetzold/dailyAPI/model/HomeEntity.java
@@ -0,0 +1,23 @@
+package tech.noetzold.dailyAPI.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.Map;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class HomeEntity {
+    private String entity_id;
+    private String state;
+    private Map<String, Object> attributes;
+    private Date last_changed;
+    private Date last_updated;
+    private Context context;
+}
