b910b42e199885ebfc82c1af8a12962d89922548
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-16 08:00:02 -0300

    Update HomeAssistantFeignClient.java

--- src/main/java/tech/noetzold/dailyAPI/client/HomeAssistantFeignClient.java
+++ src/main/java/tech/noetzold/dailyAPI/client/HomeAssistantFeignClient.java
@@ -4,7 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import tech.noetzold.dailyAPI.model.HomeEntity;
-import tech.noetzold.dailyAPI.model.dto.TrendingMoviesResponse;
 
 import java.util.List;
 
