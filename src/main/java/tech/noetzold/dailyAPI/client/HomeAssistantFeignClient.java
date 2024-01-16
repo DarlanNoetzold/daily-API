a605657a770d0826745d22c691e399668c67d9db
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-15 21:17:48 -0300

    Update HomeAssistantFeignClient.java

--- src/main/java/tech/noetzold/dailyAPI/client/HomeAssistantFeignClient.java
+++ src/main/java/tech/noetzold/dailyAPI/client/HomeAssistantFeignClient.java
@@ -3,13 +3,17 @@ package tech.noetzold.dailyAPI.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import tech.noetzold.dailyAPI.model.HomeEntity;
 import tech.noetzold.dailyAPI.model.dto.TrendingMoviesResponse;
+
+import java.util.List;
 
 @FeignClient(name = "homeAssistantClient", url = "http://192.168.18.75:8123/api")
 public interface HomeAssistantFeignClient {
 
     @GetMapping("/")
     String isHomeAssistentUp(@RequestHeader("Authorization") String bearerToken);
-    @GetMapping("/trending/movie/day")
-    TrendingMoviesResponse getTrendingMovies(@RequestHeader("Authorization") String bearerToken);
+
+    @GetMapping("/states")
+    List<HomeEntity> getStates(@RequestHeader("Authorization") String bearerToken);
 }
