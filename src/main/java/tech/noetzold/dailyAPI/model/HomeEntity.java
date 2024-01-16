e185cc02fe538d83b289af9e52ae70e7b76d624a
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-16 08:00:15 -0300

    Update HomeEntity.java

--- src/main/java/tech/noetzold/dailyAPI/model/HomeEntity.java
+++ src/main/java/tech/noetzold/dailyAPI/model/HomeEntity.java
@@ -1,6 +1,8 @@
 package tech.noetzold.dailyAPI.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,5 +21,7 @@ public class HomeEntity {
     private Map<String, Object> attributes;
     private Date last_changed;
     private Date last_updated;
+
+    @OneToOne(cascade = CascadeType.ALL)
     private Context context;
 }
