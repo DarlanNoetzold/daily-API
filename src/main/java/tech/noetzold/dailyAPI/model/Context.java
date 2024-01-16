231b039e71d6e72b23034bccc7d4ac276e43f7cb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-16 08:00:07 -0300

    Update Context.java

--- src/main/java/tech/noetzold/dailyAPI/model/Context.java
+++ src/main/java/tech/noetzold/dailyAPI/model/Context.java
@@ -1,5 +1,15 @@
 package tech.noetzold.dailyAPI.model;
 
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Context {
     private String id;
     private String parent_id;
