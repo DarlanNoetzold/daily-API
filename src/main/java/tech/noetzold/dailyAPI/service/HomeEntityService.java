13af56be3f4b8340dc1022f198e81b97ea5493c5
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-17 18:26:14 -0300

    Update HomeEntityService.java

--- src/main/java/tech/noetzold/dailyAPI/service/HomeEntityService.java
+++ src/main/java/tech/noetzold/dailyAPI/service/HomeEntityService.java
@@ -1,10 +1,14 @@
 package tech.noetzold.dailyAPI.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import tech.noetzold.dailyAPI.client.HomeAssistantFeignClient;
+import tech.noetzold.dailyAPI.model.HomeEntity;
 import tech.noetzold.dailyAPI.repository.HomeEntityRepository;
+
+import java.util.List;
 
 @Service
 public class HomeEntityService {
@@ -20,5 +24,13 @@ public class HomeEntityService {
 
     public String isUp(){
         return homeAssistantFeignClient.isHomeAssistentUp(token);
+    }
+
+    @Transactional
+    public List<HomeEntity> getAllEntities(){
+        List<HomeEntity> entities = homeAssistantFeignClient.getStates(token);
+        homeEntityRepository.saveAll(entities);
+
+        return entities;
     }
 }
