ea9e302d7509210786823a18da8dbfcbe697650f
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-17 08:01:05 -0300

    Update HomeEntity.java

--- src/main/java/tech/noetzold/dailyAPI/model/HomeEntity.java
+++ src/main/java/tech/noetzold/dailyAPI/model/HomeEntity.java
@@ -1,8 +1,6 @@
 package tech.noetzold.dailyAPI.model;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +14,10 @@ import java.util.Map;
 @AllArgsConstructor
 @Entity
 public class HomeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String entity_id;
     private String state;
     private Map<String, Object> attributes;
