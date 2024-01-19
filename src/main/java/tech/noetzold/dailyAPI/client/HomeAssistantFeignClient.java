11b1f96467f5c7a46fca8673cec2c4577a088feb
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-18 21:25:47 -0300

    Update HomeAssistantFeignClient.java

--- src/main/java/tech/noetzold/dailyAPI/client/HomeAssistantFeignClient.java
+++ src/main/java/tech/noetzold/dailyAPI/client/HomeAssistantFeignClient.java
@@ -1,9 +1,8 @@
 package tech.noetzold.dailyAPI.client;
 
+import io.lettuce.core.protocol.Command;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import tech.noetzold.dailyAPI.model.HomeEntity;
 
 import java.util.List;
@@ -19,4 +18,58 @@ public interface HomeAssistantFeignClient {
 
     @GetMapping("/api/states/{entityId}")
     HomeEntity getState(@RequestHeader("Authorization") String bearerToken, @RequestParam String entityId);
+
+    @PostMapping("/api/services/light/turn_on")
+    void turnOnLuz_da_sala(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_off")
+    void turnOffLuz_da_sala(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_on")
+    void turnOnLuz_do_quarto(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_off")
+    void turnOffLuz_do_quarto(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_on")
+    void turnOnLuz_da_frente(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_off")
+    void turnOffLuz_da_frente(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_on")
+    void turnOnLuz_da_cozinha(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_off")
+    void turnOffLuz_da_cozinha(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_on")
+    void turnOnLuz_da_bancada(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_off")
+    void turnOffLuz_da_bancada(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_on")
+    void turnOnLuz_da_varanda(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_off")
+    void turnOffLuz_da_varanda(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_on")
+    void turnOnLuz_do_escritorio(@RequestBody Command command);
+
+    @PostMapping("/api/services/light/turn_off")
+    void turnOffLuz_do_escritorio(@RequestBody Command command);
+
+    @PostMapping("/api/services/switch/turn_on")
+    void turnOnRegua_cozinha_soquete_1(@RequestBody Command command);
+
+    @PostMapping("/api/services/switch/turn_off")
+    void turnOffRegua_cozinha_soquete_1(@RequestBody Command command);
+
+    @PostMapping("/api/services/switch/turn_on")
+    void turnOnRegua_cozinha_soquete_2(@RequestBody Command command);
+
+    @PostMapping("/api/services/switch/turn_off")
+    void turnOffRegua_cozinha_soquete_2(@RequestBody Command command);
 }
