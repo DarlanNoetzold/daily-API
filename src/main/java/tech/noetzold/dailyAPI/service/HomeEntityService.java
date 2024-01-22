6fffd22cbaa670a1d123ae69d3253f2c10a264de
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-22 11:00:51 -0300

    Update HomeEntityService.java

--- src/main/java/tech/noetzold/dailyAPI/service/HomeEntityService.java
+++ src/main/java/tech/noetzold/dailyAPI/service/HomeEntityService.java
@@ -1,13 +1,11 @@
 package tech.noetzold.dailyAPI.service;
 
-import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import tech.noetzold.dailyAPI.client.HomeAssistantFeignClient;
 import tech.noetzold.dailyAPI.model.HomeEntity;
 import tech.noetzold.dailyAPI.model.dto.Command;
-import tech.noetzold.dailyAPI.repository.HomeEntityRepository;
 
 import java.util.List;
 
@@ -17,9 +15,6 @@ public class HomeEntityService {
     @Autowired
     private HomeAssistantFeignClient homeAssistantFeignClient;
 
-    @Autowired
-    private HomeEntityRepository homeEntityRepository;
-
     @Value("${spring.home.token}")
     private String token;
 
@@ -27,12 +22,8 @@ public class HomeEntityService {
         return homeAssistantFeignClient.isHomeAssistentUp(token);
     }
 
-    @Transactional
     public List<HomeEntity> getAllEntities(){
-        List<HomeEntity> entities = homeAssistantFeignClient.getStates(token);
-        homeEntityRepository.saveAll(entities);
-
-        return entities;
+        return homeAssistantFeignClient.getStates(token);
     }
 
     public HomeEntity getState(String entityId){
