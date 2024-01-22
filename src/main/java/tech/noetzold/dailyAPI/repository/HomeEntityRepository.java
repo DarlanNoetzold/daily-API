62589651b07ca2f366cae8d1e5689b0bf7441a32
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-01-22 11:00:49 -0300

    Delete HomeEntityRepository.java

--- src/main/java/tech/noetzold/dailyAPI/repository/HomeEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.noetzold.dailyAPI.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tech.noetzold.dailyAPI.model.HomeEntity;
-
-public interface HomeEntityRepository extends JpaRepository<HomeEntity, Long> {
-}
