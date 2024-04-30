5ef80aae732ef1004de4c5ad38c52b6c006a7a10
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-04-29 22:33:07 -0300

    Update JvmParamsEndpoint.java

--- src/main/java/tech/noetzold/dailyAPI/util/JvmParamsEndpoint.java
+++ src/main/java/tech/noetzold/dailyAPI/util/JvmParamsEndpoint.java
@@ -3,23 +3,42 @@ package tech.noetzold.dailyAPI.util;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Component;
-import java.lang.management.ManagementFactory;
-import java.lang.management.RuntimeMXBean;
+
+import java.lang.management.*;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
-@Endpoint(id = "jvmParams")     
+@Endpoint(id = "jvmDetails")
 public class JvmParamsEndpoint {
 
     @ReadOperation
-    public Map<String, Object> jvmParameters() {
+    public Map<String, Object> jvmDetails() {
+        Map<String, Object> details = new HashMap<>();
+
+        // Runtime Information
         RuntimeMXBean runtimeMXBean = ManagementFactory.getRuntimeMXBean();
-        Map<String, Object> params = new HashMap<>();
-        params.put("vmName", runtimeMXBean.getVmName());
-        params.put("vmVendor", runtimeMXBean.getVmVendor());
-        params.put("vmVersion", runtimeMXBean.getVmVersion());
-        params.put("inputArguments", runtimeMXBean.getInputArguments());
-        return params;
+        details.put("inputArguments", runtimeMXBean.getInputArguments());
+
+        // Memory Information
+        MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+        MemoryUsage heapMemoryUsage = memoryMXBean.getHeapMemoryUsage();
+        details.put("heapMemoryInit", heapMemoryUsage.getInit());
+        details.put("heapMemoryUsed", heapMemoryUsage.getUsed());
+        details.put("heapMemoryCommitted", heapMemoryUsage.getCommitted());
+        details.put("heapMemoryMax", heapMemoryUsage.getMax());
+
+        // Garbage Collector Information
+        List<GarbageCollectorMXBean> gcBeans = ManagementFactory.getGarbageCollectorMXBeans();
+        for (GarbageCollectorMXBean gcBean : gcBeans) {
+            Map<String, Object> gcDetails = new HashMap<>();
+            gcDetails.put("name", gcBean.getName());
+            gcDetails.put("collectionCount", gcBean.getCollectionCount());
+            gcDetails.put("collectionTime", gcBean.getCollectionTime());
+            details.put("gc_" + gcBean.getName(), gcDetails);
+        }
+
+        return details;
     }
 }
